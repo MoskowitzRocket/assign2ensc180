70f84358170dbf0361c5132f77b49ae430858437
Kale Moskowitz <kalebug@gmail.com>
2024-02-29 15:19:59 -0800

    commented exam_prob

--- exam_prob.m
+++ exam_prob.m
@@ -3,10 +3,11 @@ close all
 clc
 
 
-
+%grade data
 
 grades = [62 84 73 73 73 82 58 69 ...
 95 74 62 77 36 62 60 68 ...
 90 91 76 87 51 75 87 59];
 
+%call norm_data_study grade function
 norm_data_study(grades);
