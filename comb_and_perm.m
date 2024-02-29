64e4a89fbd8cc7104129cc77c51a185c0cf15437
Kale Moskowitz <kalebug@gmail.com>
2024-02-29 15:16:08 -0800

    Added comments to comb_perrm.m file
    should be finished now

--- comb_and_perm.m
+++ comb_and_perm.m
@@ -2,18 +2,21 @@ clear all
 close all
 clc
 
+%create empty arrays to load data into as it is calculated
 n_results = [];
 k_results = [];
 c_nk = [];
 p_nk = [];
 
+%n = 2:5
 for n = 2:5
+    %k = 0:n for each n so we loop through for each n value
     for k = 0:n
-
+        %combination values
         combination = nchoosek(n,k);
-
+        %P(n,k)
         permutations = factorial(n)/factorial(n-k);
-
+        %load results into empty arrays
         n_results = [n_results; n];
         k_results = [k_results; k];
         c_nk = [c_nk; combination];
@@ -21,10 +24,10 @@ for n = 2:5
         
     end
 end
-
+%create table of results with proper names.
 T = table(n_results,k_results,c_nk,p_nk,'VariableNames',{'n','k','C(n,k)','P(n,k)'});
 
 
 disp(T);
-%test
+
 
