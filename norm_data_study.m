75ca930ea656bad3a69c013b0b3bdd859bcf0f0a
Kale Moskowitz <kalebug@gmail.com>
2024-02-29 15:24:05 -0800

    added comments to norm_data_study.m and prob_audio.m

--- norm_data_study.m
+++ norm_data_study.m
@@ -1,19 +1,25 @@
 function norm_data_study(data)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-histogram(data)
+%create histogram of data
 
+h = histogram(data)
+%find mean of data
 mean_data = mean(data)
+%find standard deviation of data
 std_data = std(data,1)
 
+
+%find amount within 1,2,3 times standard deviation
 within_1_std = sum(abs(data - mean_data) <= std_data) / length(data)
 within_2_std = sum(abs(data - mean_data) <= 2*std_data) / numel(data)
 within_3_std = sum(abs(data - mean_data) <= 3*std_data) / numel(data)
 std_probabilities = [within_1_std within_2_std within_3_std]
 
-
+%find normal probabilities
 norm_probabilities = [normcdf(1) - normcdf(-1), normcdf(2) - normcdf(-2), normcdf(3) - normcdf(-3)]
 
+%find absolute error
 abs_error = abs(norm_probabilities - std_probabilities)
 
 
--- prob_audio.m
+++ prob_audio.m
@@ -1,11 +1,17 @@
+
+%load sound and play it
+
 load handel.mat
 sound(y, Fs);
 
-
+%plot the data from the audio sample
 plot(y)
 
+%create new figure and plot specific segment of audio sample for improved
+%clarity
 figure();
 plot(y(4000:4300))
 
+%create new figure and call norm_dist_study of the audio sample
 figure();
 norm_data_study(y)
